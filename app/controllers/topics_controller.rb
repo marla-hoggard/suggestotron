6eb81d151e9a4cf7d32d9f2ff7fb7db1df4391cc
Marla Glasser <mbglasser@gmail.com>
2018-04-10 10:53:17 -0400

    Add downvote functionality.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -67,6 +67,14 @@ class TopicsController < ApplicationController
     redirect_to(topics_path)
   end
 
+  def downvote
+    @topic = Topic.find(params[:id])
+    if @topic.votes.count > 0
+      @topic.votes.first.destroy
+    end
+    redirect_to(topics_path)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
 			post 'upvote'
 		end
 	end
+
+	resources :topics do
+		member do
+			post 'downvote'
+		end
+	end
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
